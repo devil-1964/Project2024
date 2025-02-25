eea2920dffc3c5d320140c4b3ffccb75abe95da8
Devansh <devgoe5115@gmail.com>
2025-02-25 20:23:37 +0530

    changes done

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const placementRoutes=require("./routes/placementRoutes")
 const app=express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL], 
+    origin:  process.env.FRONTEND_URL, 
     credentials: true, 
 }))
 
