eea71392281f1c02fa8f3f1e3b12e6ec778038d4
Devansh <devgoe5115@gmail.com>
2025-02-26 13:28:54 +0530

    changes done

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Keep this unless your app is under /client
+  base: "/client",
   build: {
-    outDir: "dist", // Ensure this matches Vercel's output directory
+    outDir: "dist",
   }
 });
